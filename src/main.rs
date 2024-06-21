b995b9a357b7fe68eeeeb7ea6e561b357f197f50
lrshsl <hoeslilars@gmail.com>
2024-06-21 22:47:03 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ fn main() {
         // Check if command is known
         let (cmd_name, _cmd_args) = input.split_once(' ').unwrap_or_else(|| (input.trim(), ""));
         match cmd_name {
+            "exit" => break,
             _ => println!("{cmd_name}: command not found"),
         }
     }
