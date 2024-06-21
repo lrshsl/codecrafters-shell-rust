dd40141e4f07b9354f3d25980d91c15c27c9e2fb
lrshsl <hoeslilars@gmail.com>
2024-06-21 23:28:14 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -11,14 +11,32 @@ fn main() {
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
 
-        // Check if command is known
-        let (cmd_name, cmd_args) = input.split_once(' ').unwrap_or((&input, ""));
-        let cmd_name = cmd_name.trim();
-        let cmd_args = cmd_args.trim();
+        let input_split: Vec<&str> = input
+            .split(char::is_whitespace)
+            .filter(|&s| s != "")
+            .collect();
+        if input_split.len() < 1 {
+            break;
+        }
+        let cmd_name = input_split[0];
 
         match cmd_name {
             "exit" => break,
-            "echo" => println!("{cmd_args}"),
+            "echo" => println!(
+                "{}",
+                input
+                    .split_once(char::is_whitespace)
+                    .expect("Something's fishy")
+                    .1
+                    .trim()
+            ),
+            "type" => {
+                let first_arg = *input_split.get(1).expect("Type called without arguments");
+                match first_arg {
+                    "type" | "echo" | "exit" => println!("{} is a shell builtin", first_arg),
+                    _ => println!("{}: not found", first_arg),
+                }
+            }
             _ => println!("{cmd_name}: command not found"),
         }
     }
