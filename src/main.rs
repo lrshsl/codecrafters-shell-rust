ede409b44341376c20c204cea8d2cdd25e373eec
lrshsl <hoeslilars@gmail.com>
2024-06-21 22:35:05 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,4 +9,10 @@ fn main() {
     let stdin = io::stdin();
     let mut input = String::new();
     stdin.read_line(&mut input).unwrap();
+
+    // Check if command is known
+    let (cmd_name, _cmd_args) = input.split_once(' ').unwrap_or_else(|| (input.trim(), ""));
+    match cmd_name {
+        _ => println!("{cmd_name}: command not found"),
+    }
 }
