c69a942c69299e06500a5037bf45a0c39efcf562
lrshsl <hoeslilars@gmail.com>
2024-07-04 14:49:17 +0200

    codecrafters submit [skip ci]

--- src/builtins/cd.rs
+++ src/builtins/cd.rs
@@ -1,13 +1,22 @@
+pub fn get_user_home() -> String {
+    let home = &env!("HOME");
+    if home.is_empty() {
+        println!("HOME not set");
+        "/".to_string()
+    } else {
+        home.to_string()
+    }
+}
+
 pub fn cmd_cd(input_split: Vec<&str>) {
-    let arg1 = *input_split.get(1).unwrap_or_else(|| {
-        let home = &env!("HOME");
-        if home.is_empty() {
-            println!("HOME not set");
-            &"/"
-        } else {
-            &home
-        }
-    });
-    std::env::set_current_dir(arg1)
+    let user_home_directory = get_user_home();
+    let arg1 = match input_split.get(1) {
+        // Path is supplied
+        Some(path) => path.replace('~', &user_home_directory).to_string(),
+
+        // Only `cd` -> go to user home
+        None => user_home_directory,
+    };
+    std::env::set_current_dir(&arg1)
         .unwrap_or_else(|_| println!("{arg1}: No such file or directory"));
 }
